363832ba89b87112ba0e0aa244d9ee17bbcba11f
koo5 <kolman.jindrich@gmail.com>
2020-09-24 22:35:49 +0200

    ..

--- brn/brn/tau_testcase_parser.py
+++ brn/brn/tau_testcase_parser.py
@@ -1,6 +1,9 @@
 import logging
-from locators import *
+import shlex
 import pathlib
+from enum import Enum, auto
+from locators import *
+from dotdict import Dotdict
 
 def find_all_files_recursively(path: Path):
 	paths = []
@@ -17,14 +20,6 @@ def element_by_index_upper_clipped(array, index):
 		return array[-1]
 
 
-
-
-query_counter = 0
-input_text = ""
-
-
-from enum import Enum, auto
-
 class Mode(Enum):
 	COMMANDS = auto()
 	KB = auto()
@@ -32,47 +27,45 @@ class Mode(Enum):
 	SHOULDBE = auto()
 
 
-format = "";
-base = "";
-
 
 def parse_testcase(p: Path):
 	fn = p.value
 	logging.getLogger(__name__).info(f'parsing {fn}')
 	assert isinstance(fn, pathlib.PosixPath)
-	c = Context()
-	c.set_setting('mode', Mode.COMMANDS)
-	c.set_setting('result_limit', 123)
 	with fn.open() as f:
-		c.input = f
-		input.interpret(f)
+		c = Context(f)
+		c.set_mode(Mode.COMMANDS)
+		c.set_setting('result_limit', 123)
+		c.interpret()
 
-import shlex
 
 class Context:
-	def __init__(self):
-		self.mode_stack = [Mode.COMMANDS]
+	"""
+	format = "";
+	base = "";
+	"""
+	def __init__(self, input):
+		self.input = input
+		self.mode_stack = []
+		self.data = {'testcases': []}
+		self.settings = Dotdict()
 
-	def set_setting(k,v):
+	def set_mode(self, mode):
+		self.mode_stack.push(mode)
+		self.print_setting('mode', self.mode)
+
+	def set_setting(self, k, v):
+		self.print_setting(k,v)
+		self.settings[k] = v
+
+	def self.print_setting(self, k,v):
 		logging.getLogger(__name__).info(f'#{k} = {human_friendly_setting_value(v)}')
-		self.setting[k] = v
 
 	def human_friendly_setting_value(x):
 		try:
 			return x.name
 		except:
 			return x
-
-	def pop_token(line:str):
-		line = line.strip()
-		if not len(line):
-			return ''
-		tokens = shlex.split(line)
-		tokens[0]
-
-
-
-
 
 	def interpret():
 		while l = f.readline():
@@ -82,7 +75,7 @@ class Context:
 			else:
 				l2 = l.strip()
 				if l2 == 'fin.':
-					on_complete_rdf_text()
+					self.on_complete_rdf_text()
 					self.mode_stack.pop()
 				else:
 					self.rdf_lines.append(l)
@@ -91,14 +84,14 @@ class Context:
 		while len(self.tokens):
 			token = self.tokens.pop(0)
 
-	def on_complete_rdf_text(self, data):
+	def on_complete_rdf_text(self):
 		text = '\n'.join(self.rdf_lines)
 		print('on_complete_rdf_text ' + text)
 		if self.mode == Mode.KB:
 			self.kb_text = text
 		if self.mode == Mode.QUERY:
 			tc = Dotdict()
-			data.testcases.append(tc)
+			self.data.testcases.append(tc)
 			self.last_textcase = tc
 			tc.type = PYCO.query
 			tc.kb_text = self.kb_text
